d1e111f84118f2cececfcddd753744c96e9f18a9
MuzammilMusthafa <adi95949155@gmail.com>
2024-07-17 11:37:43 +0530

    Add files via upload

--- muzammil1.py
+++ muzammil1.py
@@ -50,7 +50,7 @@ for value in range(input6):
    **
    ***
    ****
-   **** '''
+   ***** '''
 list(range(5,0,-1))    
 5*'*'
 for value in range(0,7):
@@ -66,17 +66,47 @@ for value in range(0,7):
 for value in range(0,7):
     print(value*'*',end =' ')
 
-for row in range(1,7):
+
+    for row in range(1,7):
     for col in range(1,row):
-        print(col,end='')
-    print()     
+        print(col,end='') 
+    print()        
 
 for row in range(7,1,-1):
     for col in range(1,row):
         print(col,end='')
     print()    
 
+'''
+   *
+   **
+   ***
+   ****
+   *****
+   ****** '''
+for row in range(1,8):
+    for col in range(1,row):
+        print('*',end='')
+    print()    
 
+'''                 
+    *
+   ***
+  *****
+ *******
+*********'''
+for row in range(1,6):
+    for col in range(6,row,-1):
+        print(' ',end=' ')
+      
+    for col in range(0,row*2-1):
+        print('*',end=' ')
+    print()
 
+for row in range(1,6):
+    for col in range (6,row,-1):
+        print(' ',end='')
 
-    
+    for col in range(0,row*2-1):
+        print('*',end='')
+    print()    
